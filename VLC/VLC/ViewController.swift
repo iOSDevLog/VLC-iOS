ea4f7add1228e7d6b5e06922f2a41ca43cdb55f9
iosdevlog <iosdevlog@iosdevlog.com>
2019-07-16 09:50:56 +0800

    feat(MobileVLCKit): cocoapods

---
+++ VLC/VLC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VLC
+//
+//  Created by developer on 7/16/19.
+//  Copyright © 2019 iOSDevLog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
